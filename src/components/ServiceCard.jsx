58f25e6fc8ccb75731cbf9fad93e670cf1a5b064
Evans <www.evanschauke@gmail.com>
2025-11-11 18:02:33 +0300

    mobile responsiveness

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,3 +1,12 @@
 import React from 'react'
 import '../styles/cards.css'
-export default function ServiceCard({title,description,image}){return(<article className="card"><img src={image} alt={title} loading="lazy"/><h3>{title}</h3><p>{description}</p></article>)}
+
+export default function ServiceCard({ title, description, image }) {
+  return (
+    <article className="card">
+      <img src={image} alt={title} loading="lazy" decoding="async" />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </article>
+  )
+}
